e759d98d454a632ac3bca52f801a52c5261d949d
Patrick Pudlo <patrick.pudlo92@gmail.com>
2023-08-04 15:42:53 +0200

    Adding DashboardHeader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,22 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <div className="overflow-hidden bg-no-repeat bg-cover rounded-xl dashboard-header">
+        <div className="top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed dashboard-background">
+          <div className="flex justify-center items-center h-full">
+            <div className="text-center text-white lg:px-6 md:px-12 px-2">
+              <h1 className="text-5xl font-bold mt-12 mb-12">Das passiert in Westerloy</h1>
+              <h3 className="text-3xl font-bold lg:mb-24 mb-8">Auf einen Blick alle Events in und rund um das schönste Dorf des Ammerlands.</h3>
+              <button
+                type="button"
+                className="inline-block px-6 py-2.5 border-2 border-white text-white font-medium text-md leading-tight uppercase rounded hover:bg-black hover:bg-opacity-45 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                data-mdb-ripple="true" data-mdb-ripple-color="light">
+                Zu den Events
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
