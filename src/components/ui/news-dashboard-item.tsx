1c7234fdda026215bac9dd1418402d3f0dcf1ae4
Patrick Pudlo <patrick.pudlo92@gmail.com>
2023-08-07 11:54:47 +0200

    now with working casing. Also fixed react keywords in svg tag

--- src/components/ui/news-dashboard-item.tsx
+++ src/components/ui/news-dashboard-item.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-export default function newsDashboardItem() {
+export default function NewsDashboardItem() {
     return (
         <div className="flex flex-col lg:max-w-lg lg:max-h-lg max-h-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
             <Image className="rounded-t-lg h-3/4" src="" width={800} height={600} alt="" />
@@ -9,7 +9,7 @@ export default function newsDashboardItem() {
                 <p className="mb-3 mt-2 mx-6 font-normal text-gray-700 dark:text-gray-400">Dies ist ein Test-Eintrag zum testen des Designs</p>
                 <button className="inline-flex self-end items-center pr-6 py-2 text-sm font-medium text-center text-black rounded-lg">
                     <p>Zum Artikel</p>
-                    <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 </button>
             </div>
         </div>
