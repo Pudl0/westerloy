8459b3a12a42c87ecfe44f0b03cecfa0d6a54c05
Patrick Pudlo <patrick.pudlo92@gmail.com>
2023-08-15 15:34:20 +0200

    Adding newsdetail route

---
+++ src/app/newsdetail/page.tsx
@@ -0,0 +1,9 @@
+import NewsDetailComponent from "@/components/news-detail-component";
+
+export default function NewsDetail() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-10">
+        <NewsDetailComponent></NewsDetailComponent>
+      </main>
+    )
+  }
