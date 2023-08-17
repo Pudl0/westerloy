e6e086d5fe1d8cdce503cc28dde24856f61f1171
Patrick Pudlo <patrick.pudlo92@gmail.com>
2023-08-17 15:34:24 +0200

    Adding testimage

--- src/components/ui/news-dashboard-item.tsx
+++ src/components/ui/news-dashboard-item.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function NewsDashboardItem() {
     return (
         <div className="flex flex-col lg:max-w-lg lg:max-h-lg max-h-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-            <Image className="rounded-t-lg h-3/4" src="" width={800} height={600} alt="" />
+            <Image className="rounded-t-lg h-3/4" src="/Muehlenhof.jpg" width={800} height={600} alt="" />
             <div className="px-2 py-3 flex flex-col">
                 <h5 className="mb-2 pl-6 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Testeintrag</h5>
                 <p className="mb-3 mt-2 mx-6 font-normal text-gray-700 dark:text-gray-400">Dies ist ein Test-Eintrag zum testen des Designs</p>
