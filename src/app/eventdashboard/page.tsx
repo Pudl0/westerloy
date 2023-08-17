f9cb720ccd55a73dc8d2396aaf60e092d8c29c9b
Patrick Pudlo <patrick.pudlo92@gmail.com>
2023-08-17 12:42:55 +0200

    Adding EventDashboard Route

---
+++ src/app/eventdashboard/page.tsx
@@ -0,0 +1,10 @@
+import EventDashboardItem from "@/components/ui/event-dashboard-item";
+
+export default function EventDashboard() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between gap-y-24 p-10">
+        <EventDashboardItem></EventDashboardItem>
+        <EventDashboardItem></EventDashboardItem>
+    </main>
+  )
+}
