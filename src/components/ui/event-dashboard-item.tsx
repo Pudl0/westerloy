d8761b25b99cd71bebe82036931395d0b0e0c6aa
Patrick Pudlo <patrick.pudlo92@gmail.com>
2023-08-18 14:12:09 +0200

    Fixing html tag errors inside of svg

--- src/components/ui/event-dashboard-item.tsx
+++ src/components/ui/event-dashboard-item.tsx
@@ -15,7 +15,7 @@ export default function EventDashboardItem() {
                     <p className="leading-normal">Wird wieder n mordsmäßiges Spiel</p>
                     <div className="flex flex-row items-center mt-4 text-gray-700">
                         <div className="w-1/2 flex gap-2">
-                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
                             LVM Arena
                         </div>
                     </div>
