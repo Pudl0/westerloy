a1b98c7c3857d61d7c9fc4cfb94e9bb53c5333ce
Pudl0 <63001666+Pudl0@users.noreply.github.com>
2023-08-22 18:26:10 +0200

    removing unneeded component since all the stuff can be on the page.tsx

--- src/app/newsdetail/page.tsx
+++ src/app/newsdetail/page.tsx
@@ -1,9 +1,42 @@
-import NewsDetailComponent from '@/components/news-detail-component';
+import Image from 'next/image';
+import { PrismaClient } from '@prisma/client';
 
-export default function NewsDetail() {
+const prisma = new PrismaClient();
+export default async function NewsDetail() {
+  const newsEntry = await prisma.NewsEntries.findUniqueOrThrow({
+    where: {
+      Id: 6,
+    },
+  });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-10">
-      <NewsDetailComponent></NewsDetailComponent>
-    </main>
+    <div className="mt-10 w-full">
+      <div className="mb-4 md:mb-0 w-full max-w-screen-md mx-auto relative" style={{ height: 24 + 'em' }}>
+        <div className="absolute left-0 bottom-0 w-full h-full z-10 rounded-xl news-item-header"></div>
+        <Image
+          src="/Muehlenhof.jpg"
+          width={1920}
+          height={1080}
+          alt=""
+          className="absolute left-0 top-0 w-full h-full z-0 object-cover rounded-xl"
+        />
+        <div className="p-4 absolute bottom-0 left-0 z-20">
+          <h2 className="text-4xl font-semibold text-gray-100 leading-tight">{newsEntry.Title}</h2>
+        </div>
+      </div>
+      <div className="mt-12 text-gray-700 max-w-screen-md mx-auto text-lg leading-relaxed">{newsEntry.Description}</div>
+    </div>
   );
 }
+
+NewsDetail()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+
+  .catch(async (e) => {
+    console.error(e);
+
+    await prisma.$disconnect();
+
+    process.exit(1);
+  });
