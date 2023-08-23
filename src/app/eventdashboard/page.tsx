483eb8b36474036e7632cec119f05ac8a1ee9167
Patrick Pudlo <patrick.pudlo92@gmail.com>
2023-08-23 09:12:07 +0200

    removed padding from event-dashboard page

--- src/app/eventdashboard/page.tsx
+++ src/app/eventdashboard/page.tsx
@@ -2,7 +2,7 @@ import EventDashboardItem from '@/components/cards/event-dashboard-item';
 
 export default function EventDashboard() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between gap-y-24 p-10">
+    <main className="flex min-h-screen flex-col items-center justify-between gap-y-24">
       <EventDashboardItem></EventDashboardItem>
       <EventDashboardItem></EventDashboardItem>
     </main>
