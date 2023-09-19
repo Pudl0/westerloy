5438f9b1e0f898ed6a0039786510f3f78416f204
Pudl0 <63001666+Pudl0@users.noreply.github.com>
2023-09-19 06:04:47 +0200

    applying prisma conventions to prevent breaking with migration/db changes

--- src/app/eventdashboard/page.tsx
+++ src/app/eventdashboard/page.tsx
@@ -5,7 +5,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default async function EventDashboard() {
-  const eventEntries = await prisma.evententries.findMany();
+  const eventEntries = await prisma.eventEntries.findMany();
   return (
     <div className="flex min-h-screen flex-col items-center justify-between gap-y-24 my-12">
       {eventEntries.map(function (eventEntry: EventEntry) {
--- src/app/newsdetail/[newsdetailid]/page.tsx
+++ src/app/newsdetail/[newsdetailid]/page.tsx
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default async function NewsDetail({ params }: { params: { newsdetailid: string } }) {
-  const newsEntry = await prisma.NewsEntries.findUniqueOrThrow({
+  const newsEntry = await prisma.newsEntries.findUniqueOrThrow({
     where: {
       id: parseInt(params.newsdetailid),
     },
