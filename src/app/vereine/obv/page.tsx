8924e5a1635ada2dda743a3ca63aadc6674b5d6e
Patrick Pudlo <63001666+Pudl0@users.noreply.github.com>
2023-10-13 15:42:16 +0200

    applying auto format

--- src/app/vereine/obv/page.tsx
+++ src/app/vereine/obv/page.tsx
@@ -17,16 +17,17 @@ export default async function OBV() {
         </div>
       </div>
       <div className="mx-auto mt-12 max-w-screen-md text-lg leading-relaxed text-gray-700">
-      <p className="pb-6">
-            Seit mehr als einem halben Jahrhundert setzen wir uns engagiert als Ortsbürgerverein Westerloy für die Belange
-            unserer Dorfbewohner*innen ein. Wir möchten ein lebendiges Miteinander von Jung & Alt unterstützen und das Lokale fördern.
+        <p className="pb-6">
+          Seit mehr als einem halben Jahrhundert setzen wir uns engagiert als Ortsbürgerverein Westerloy für die Belange
+          unserer Dorfbewohner*innen ein. Wir möchten ein lebendiges Miteinander von Jung & Alt unterstützen und das
+          Lokale fördern.
         </p>
         <p className="pb-6">
-            Mit zahlreichen tollen Veranstaltungen und Aktionen auch zusammen mit unserer Landjugend beleben wir unser Dorf
-            rund um unseren Dorfmittelpunkt - unseren Mühlenhof - unser Dorfgemeinschaftshaus.
+          Mit zahlreichen tollen Veranstaltungen und Aktionen auch zusammen mit unserer Landjugend beleben wir unser
+          Dorf rund um unseren Dorfmittelpunkt - unseren Mühlenhof - unser Dorfgemeinschaftshaus.
         </p>
         <p className="pb-6">
-            Wir freuen uns sehr, wenn auch du unser Dorfleben mitgestalten möchtest, Anregungen oder Kritik hast.
+          Wir freuen uns sehr, wenn auch du unser Dorfleben mitgestalten möchtest, Anregungen oder Kritik hast.
         </p>
       </div>
     </div>
