15c344560efa9a20f1c41a0bee5d37f1a3323a53
Pudl0 <patrick.pudlo92@gmail.com>
2023-12-28 13:46:08 +0100

    need to have seperate file pahtes because of next/image component conventions

--- src/app/api/newsentries/route.tsx
+++ src/app/api/newsentries/route.tsx
@@ -10,12 +10,16 @@ export async function POST(req: Request) {
   var data = pictureString.replace(/^data:image\/\w+;base64,/, '');
   var buffer = Buffer.from(data, 'base64');
   var imagePath = './public/uploads/news/'; // Update the path to the desired location relative to the public directory
-  const filePath = imagePath + title + '.jpg';
+  const fileName = title + '.jpg';
+  const filePath = imagePath + fileName;
+  const savePath = '/uploads/news/' + fileName; // Update the path to the desired location relative to the public directory
+
   await fs.promises.writeFile(filePath, buffer);
-  var savePath = '/uploads/news/' + title + '.jpg'; // Update the path to the desired location relative to the public directory
+
   await prisma.newsEntries.create({
     data: { title, description, pictureLink: savePath, shortDescription },
   });
+
   await prisma.$disconnect();
   return NextResponse.json({ message: 'Created new NewsEntry' }, { status: 200 });
 }
