ce1272174d3d22a741c6473bda18d7992091c023
Pudl0 <patrick.pudlo92@gmail.com>
2024-01-05 10:18:03 +0100

    improving gallery layout

--- src/app/dorfgeschehen/recap/[recapdetailid]/page.tsx
+++ src/app/dorfgeschehen/recap/[recapdetailid]/page.tsx
@@ -32,22 +32,32 @@ export default async function RecapDetail({ params }: { params: { recapdetailid:
       <div className="mx-auto mt-12 max-w-screen-md text-lg leading-relaxed text-gray-700">
         {recapentry.description}
       </div>
-      <div className="flex h-full w-full gap-4">
-        <p className="text-2xl font-semibold">Impressionen</p>
-        {fileNames.map(function (fileName: string, index: number) {
-          return (
-            <div key={index} className="pb-3/4 relative h-full w-full">
+      <div className="flex min-h-screen flex-col items-center justify-between px-10">
+        <div className="md:pb-12 md:pt-24">
+          <h2 className="my-8 flex flex-row flex-nowrap items-center">
+            <span
+              className="text-md mx-2 block flex-none bg-black px-2
+                    py-1.5 font-medium uppercase leading-none
+                    text-white lg:mx-4 lg:px-4 lg:py-2.5 lg:text-xl"
+            >
+              Impressionen
+            </span>
+          </h2>
+        </div>
+        <div className="m-4 grid grid-cols-2 flex-wrap justify-center gap-x-11">
+          {fileNames.map(function (fileName: string) {
+            return (
               <Image
                 key={fileName}
                 src={`/uploads/recap/${recapentry.title}` + '/' + fileName}
                 width={1920}
                 height={1080}
                 alt=""
-                className="absolute left-0 top-0 z-0 h-full w-full rounded-xl object-cover"
+                className="w-full rounded-xl object-cover"
               />
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
