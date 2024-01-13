9e6344acdf9153d0114a4caaf86041a65fa8110c
Pudl0 <patrick.pudlo92@gmail.com>
2024-01-13 15:13:46 +0100

    removing header image bug

--- src/app/vereine/landjugend/page.tsx
+++ src/app/vereine/landjugend/page.tsx
@@ -4,13 +4,13 @@ export default async function Landjugend() {
   return (
     <div className="mt-10 w-full">
       <div className="relative mx-auto mb-4 w-full max-w-screen-md md:mb-0" style={{ height: 24 + 'em' }}>
-        <div className="news-item-header absolute bottom-0 left-0 z-10 h-full w-full rounded-xl"></div>
+        <div className="news-item-header absolute bottom-0 left-0 h-full w-full rounded-xl"></div>
         <Image
           src="/Landjugend.jpg"
           width={1920}
           height={1080}
           alt=""
-          className="absolute left-0 top-0 z-0 h-full w-full rounded-xl object-cover"
+          className="absolute left-0 top-0 h-full w-full rounded-xl object-cover"
         />
         <div className="absolute bottom-0 left-0 z-20 p-4">
           <h2 className="text-4xl font-semibold leading-tight text-gray-100">Landjugend 3.0</h2>
