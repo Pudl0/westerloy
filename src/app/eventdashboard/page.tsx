68b993180f1842d5343f29874a59ee90bac2a713
Pudl0 <patrick.pudlo92@gmail.com>
2024-04-02 22:02:47 +0200

    adding eventDashboard to mainpage and adjusting the component

--- src/app/eventdashboard/page.tsx
+++ src/app/eventdashboard/page.tsx
@@ -1,5 +1,4 @@
 import EventDashboardItem from '@/components/cards/event-dashboard-item';
-import BackToDashboardButton from '@/components/ui/back-to-dashboard-button';
 import { EventEntry } from '@/lib/types/event-entry';
 import { PrismaClient } from '@prisma/client';
 import { unstable_noStore as noStore } from 'next/cache';
@@ -15,7 +14,6 @@ export default async function EventDashboard() {
   prisma.$disconnect();
   return (
     <div className="my-12 flex min-h-screen flex-col items-center justify-between gap-y-24">
-      <BackToDashboardButton></BackToDashboardButton>
       {eventEntries.map(function (eventEntry: EventEntry) {
         return <EventDashboardItem eventEntry={eventEntry} key={eventEntry.id} />;
       })}
