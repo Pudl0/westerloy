0afd97dd11c22a5020154b3ba1a1860eaac5cfba
Pudl0 <patrick.pudlo92@gmail.com>
2024-06-20 14:34:27 +0200

    adjusted fonts of loginmenu to fit new navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -156,6 +156,7 @@ const LoggedInMenu = () => {
           pathname: '/editor',
         }}
         as={'/editor'}
+        className="text-sm font-medium"
       >
         Editor
       </Link>
--- src/components/ui/loginbutton.tsx
+++ src/components/ui/loginbutton.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function LoginButton() {
   return (
     <Link
-      className="hover:underline"
+      className="text-sm font-medium hover:underline"
       href={{
         pathname: '/api/auth/signin',
       }}
