8b243d518f1540fd2c04c4967e401013772d8413
Pudl0 <patrick.pudlo92@gmail.com>
2024-07-06 21:01:54 +0200

    introducing own components for eventdashboard segments

---
+++ src/components/dashboard-divider.tsx
@@ -0,0 +1,15 @@
+export default function DashboardDivider() {
+  return (
+    <div className="md:pb-12 md:pt-24">
+      <h2 className="my-8 flex flex-row flex-nowrap items-center">
+        <span
+          className="text-md mx-2 block flex-none bg-black px-2
+                py-1.5 font-medium uppercase leading-none
+                text-white lg:mx-4 lg:px-4 lg:py-2.5 lg:text-xl"
+        >
+          Aktuelle Veranstaltungen
+        </span>
+      </h2>
+    </div>
+  );
+}
---
+++ src/components/dashboard-header.tsx
@@ -0,0 +1,13 @@
+export default function DashboardHeader() {
+  return (
+    <div className="dashboard-header max-h-3/4 my-6 overflow-hidden rounded-xl bg-cover bg-no-repeat lg:w-3/4">
+      <div className="dashboard-background bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed">
+        <div className="flex h-full items-center justify-center">
+          <div className="px-2 text-center text-white md:px-12 lg:px-6">
+            <h1 className="mb-12 mt-12 text-3xl font-bold lg:text-5xl">Herzlich Willkommen in Westerloy</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
