c26680b09d8ca579e394fa4634775b6579963aa2
Pudl0 <patrick.pudlo92@gmail.com>
2024-07-08 07:34:30 +0200

    adding newsEntry dummy data, introducing constant to prevent seed file from bloating up and improving readme

---
+++ prisma/dev-seed.ts
@@ -0,0 +1,65 @@
+import { loremIpsum150 } from '@/lib/utils/dummy-data-constants';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  //These are the EventEntry dummy objects. Add oder edit them to your needs.
+  const testEvent1 = await prisma.eventEntries.upsert({
+    where: { id: 0 },
+    update: {},
+    create: {
+      title: 'TestEvent1',
+      description: 'Dies ist eine Testbeschreibung',
+      location: 'TestLocation',
+      timeOfEvent: new Date(Date.now() + 86400000),
+      pictureLink: '',
+    },
+  });
+  const testEvent2 = await prisma.eventEntries.upsert({
+    where: { id: 0 },
+    update: {},
+    create: {
+      title: 'TestEvent2',
+      description: 'Dies ist eine wesentlich längere Testbeschreibung, um die maximale Länge zu testen',
+      location: 'TestLocation',
+      timeOfEvent: new Date(Date.now() + 86400000 * 3),
+      pictureLink: '',
+    },
+  });
+  console.log({ testEvent1, testEvent2 });
+
+  //dummy newsEntry data. LoremIpsum constants should be change din the respextive constants-file to prevent this file from bloating up
+  const testNews1 = await prisma.newsEntries.upsert({
+    where: { id: 0 },
+    update: {},
+    create: {
+      title: 'Neue Testneuigkeiten',
+      description: 'Hier wird der Nachrichtentext stehen.',
+      shortDescription: 'Kurzbeschreibung für eine Nachricht.',
+      pictureLink: '',
+    },
+  });
+
+  const testNews2 = await prisma.newsEntries.upsert({
+    where: { id: 0 },
+    update: {},
+    create: {
+      title: 'Neue noch längere Testneuigkeiten',
+      description: loremIpsum150,
+      shortDescription: 'Kurzbeschreibung für eine längere Nachricht.',
+      pictureLink: '',
+    },
+  });
+  console.log(testNews1, testNews2);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
