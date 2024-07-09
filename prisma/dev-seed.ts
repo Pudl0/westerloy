fb8e88e23de295b7d4897a88916335936d598048
Pudl0 <patrick.pudlo92@gmail.com>
2024-07-09 21:47:49 +0200

    applying changes

--- prisma/dev-seed.ts
+++ prisma/dev-seed.ts
@@ -1,19 +1,9 @@
 import { dummyData } from '@/../prisma/dummy-data';
-import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
 const amountEventEntries = 3;
 const amountNewsEntries = 3;
 
-async function main() {
-  dummyData.seed(amountEventEntries, amountNewsEntries);
-}
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+dummyData.seed(amountEventEntries, amountNewsEntries).catch(async (e) => {
+  console.error(e);
+  process.exit(1);
+});
