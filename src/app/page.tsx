130bc84bb7572cf6a614505a7c5a1ce6503f5a8b
Pudl0 <patrick.pudlo92@gmail.com>
2024-07-11 15:23:04 +0200

    removing margins/paddings from child components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import EventDashboard from '@/components/eventdashboard';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-10">
+    <main className="flex min-h-screen flex-col items-center justify-between gap-y-12 px-4">
       <DashboardHeader />
       <DashboardDivider />
       <EventDashboard />
