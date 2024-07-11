9304cf8fff198c2dbbf4b0591827ddb63c8fa154
Pudl0 <patrick.pudlo92@gmail.com>
2024-07-11 20:58:35 +0200

    changing some gaps/padding and increasing linecount before truncate

--- src/components/cards/event-dashboard-item.tsx
+++ src/components/cards/event-dashboard-item.tsx
@@ -24,8 +24,20 @@ export default async function EventDashboardItem(props: { eventEntry: EventEntry
         <div className="grid w-1/2 place-items-center max-md:hidden lg:w-full">
           <Image src={props.eventEntry.pictureLink} alt="Eventfoto" className="rounded-xl" width={1920} height={1080} />
         </div>
-        <div className="flex w-full flex-col xl:p-3">
-          <div className="flex items-center justify-between self-end">
+        <div className="flex w-full flex-col">
+          <h3 className="text-base font-black text-black lg:text-2xl xl:pb-3">{props.eventEntry.title}</h3>
+          <p className="mb-3 line-clamp-2 overflow-hidden text-ellipsis text-base text-black md:line-clamp-2 md:text-lg lg:line-clamp-3 xl:line-clamp-5">
+            {props.eventEntry.description}
+          </p>
+          <div className="flex gap-4 lg:absolute lg:bottom-1 lg:gap-8 xl:bottom-5">
+            <div className="flex items-center">
+              <FontAwesomeIcon icon={faClock} className="fa-regular" />
+              <p className="ml-1 text-xs font-bold text-black lg:text-sm">{dateString}</p>
+            </div>
+            <div className="flex items-center">
+              <FontAwesomeIcon icon={faLocationDot} className="fa-regular" />
+              <p className="ml-1 text-xs font-bold text-black lg:text-sm">{props.eventEntry.location}</p>
+            </div>
             {session && (
               <div className="rounded-full bg-westerloySecondary px-3 py-1 text-xs font-medium text-black hover:bg-westerloyAccent max-xl:hidden">
                 <Link
@@ -45,20 +57,6 @@ export default async function EventDashboardItem(props: { eventEntry: EventEntry
                 </Link>
               </div>
             )}{' '}
-          </div>
-          <h3 className="text-base font-black text-black max-md:pb-3 lg:pb-3 lg:text-2xl">{props.eventEntry.title}</h3>
-          <p className="overflow-hidden text-ellipsis text-base text-black max-md:hidden md:line-clamp-1 md:text-lg lg:line-clamp-2 xl:line-clamp-3">
-            {props.eventEntry.description}
-          </p>
-          <div className="flex gap-4 md:absolute md:bottom-3 lg:bottom-6 lg:gap-8">
-            <div className="flex items-center">
-              <FontAwesomeIcon icon={faClock} className="fa-regular" />
-              <p className="ml-1 text-xs font-bold text-black lg:text-sm">{dateString}</p>
-            </div>
-            <div className="flex items-center">
-              <FontAwesomeIcon icon={faLocationDot} className="fa-regular" />
-              <p className="ml-1 text-xs font-bold text-black lg:text-sm">{props.eventEntry.location}</p>
-            </div>
           </div>
         </div>
       </div>
