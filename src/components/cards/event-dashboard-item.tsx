0c57524a0c97097b96cc74a16e9c56d0f1ecc027
Pudl0 <patrick.pudlo92@gmail.com>
2024-07-11 15:46:36 +0200

    applying background colors from scheme and further adjusting gaps/padding/margins

--- src/components/cards/event-dashboard-item.tsx
+++ src/components/cards/event-dashboard-item.tsx
@@ -20,14 +20,14 @@ export default async function EventDashboardItem(props: { eventEntry: EventEntry
     props.eventEntry.timeOfEvent.getFullYear();
   return (
     <div className="flex w-full flex-col justify-center">
-      <div className="relative mx-auto flex max-w-sm flex-col rounded-xl border border-white bg-white p-3 shadow-lg md:max-w-3xl md:flex-row md:space-x-5 md:space-y-0 lg:max-w-6xl lg:space-y-3">
-        <div className="grid w-1/2 place-items-center bg-white max-md:hidden lg:w-full">
+      <div className="relative mx-auto flex max-w-sm flex-col rounded-xl border bg-westerloyPrimary p-3 shadow-lg md:max-w-3xl md:flex-row md:space-x-5 md:space-y-0 lg:max-w-6xl lg:space-y-3">
+        <div className="grid w-1/2 place-items-center max-md:hidden lg:w-full">
           <Image src={props.eventEntry.pictureLink} alt="Eventfoto" className="rounded-xl" width={1920} height={1080} />
         </div>
-        <div className="flex w-full flex-col bg-white xl:p-3">
+        <div className="flex w-full flex-col xl:p-3">
           <div className="flex items-center justify-between self-end">
             {session && (
-              <div className="rounded-full bg-gray-200 px-3 py-1 text-xs font-medium text-gray-800 max-xl:hidden">
+              <div className="rounded-full bg-westerloySecondary px-3 py-1 text-xs font-medium text-black hover:bg-westerloyAccent max-xl:hidden">
                 <Link
                   href={{
                     pathname: 'editor/edit/veranstaltungen/[evententryid]',
@@ -46,18 +46,18 @@ export default async function EventDashboardItem(props: { eventEntry: EventEntry
               </div>
             )}{' '}
           </div>
-          <h3 className="pb-3 text-base font-black text-gray-800 lg:text-2xl">{props.eventEntry.title}</h3>
-          <p className="line-clamp-3 overflow-hidden text-ellipsis text-base text-gray-500 max-xl:hidden md:text-lg">
+          <h3 className="text-base font-black text-black max-md:pb-3 lg:pb-3 lg:text-2xl">{props.eventEntry.title}</h3>
+          <p className="overflow-hidden text-ellipsis text-base text-black max-md:hidden md:line-clamp-1 md:text-lg lg:line-clamp-2 xl:line-clamp-3">
             {props.eventEntry.description}
           </p>
-          <div className="flex gap-4 lg:absolute lg:bottom-9 lg:gap-8">
+          <div className="flex gap-4 md:absolute md:bottom-3 lg:bottom-6 lg:gap-8">
             <div className="flex items-center">
               <FontAwesomeIcon icon={faClock} className="fa-regular" />
-              <p className="ml-1 text-xs font-bold text-gray-600 lg:text-sm">{dateString}</p>
+              <p className="ml-1 text-xs font-bold text-black lg:text-sm">{dateString}</p>
             </div>
             <div className="flex items-center">
               <FontAwesomeIcon icon={faLocationDot} className="fa-regular" />
-              <p className="ml-1 text-xs font-bold text-gray-600 lg:text-sm">{props.eventEntry.location}</p>
+              <p className="ml-1 text-xs font-bold text-black lg:text-sm">{props.eventEntry.location}</p>
             </div>
           </div>
         </div>
