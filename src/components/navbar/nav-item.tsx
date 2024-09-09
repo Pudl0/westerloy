b1ae087023677a5165e86f89a9bc72a09fa3fb1c
Pudl0 <patrick.pudlo92@gmail.com>
2024-09-09 11:17:47 +0200

    fixing type issue

--- src/components/navbar/nav-item.tsx
+++ src/components/navbar/nav-item.tsx
@@ -2,7 +2,6 @@ import { ChevronDown } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 
-import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import {
   NavigationMenuContent,
